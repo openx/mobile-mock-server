c2c33a16e106096dc5ca60eb5ded9b8243c6a17b
Riabkov <riabkov.stanislav@gmail.com>
2020-01-16 16:58:50 +0200

    feat(MockServer): Add functionality to mock server - Possibility to set latency was added - Possibility to set response with error was added - Possibility to reset configuration was added

--- backend/urls.py
+++ backend/urls.py
@@ -8,6 +8,9 @@ urlpatterns = [
     path('api/add_mock', views.AddMockView.as_view(), name='api'),
     path('api/logs', views.EventsLogView.as_view(), name='events_log'),
     path('api/clear_logs', views.ClearLogView.as_view(), name='clear_log'),
+    path('api/clear_configs', views.ClearConfigsView.as_view(), name='clear_configs'),
+    path('api/set_error', views.SetErrorView.as_view(), name='set_error'),
+    path('api/set_latency', views.SetLatencyView.as_view(), name='set_latency'),
     path('image', views.ImageGeneratorView.as_view(), name='image'),
     re_path('events.*', views.EventsView.as_view(), name='events'),
 ]
